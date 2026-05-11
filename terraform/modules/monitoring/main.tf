7fb2daa918d0dfe28e6fd2a96c50b8645fbd9ff8
Ubuntu <ubuntu@ip-172-31-40-14.ec2.internal>
2026-05-11 11:59:19 +0000

    Remove ELK from Terraform - install manually

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -1,4 +1,3 @@
-# Add Helm repos
 resource "helm_release" "prometheus" {
   name             = "prometheus"
   repository       = "https://prometheus-community.github.io/helm-charts"
@@ -28,76 +27,4 @@ resource "helm_release" "prometheus" {
   }
 
   depends_on = [var.eks_cluster_name]
-}
-
-# ELK Stack
-resource "helm_release" "elasticsearch" {
-  name             = "elasticsearch"
-  repository       = "https://helm.elastic.co"
-  chart            = "elasticsearch"
-  namespace        = "logging"
-  create_namespace = true
-  version          = "8.5.1"
-
-  set {
-    name  = "replicas"
-    value = "1"
-  }
-
-  set {
-    name  = "minimumMasterNodes"
-    value = "1"
-  }
-
-  set {
-    name  = "resources.requests.memory"
-    value = "512Mi"
-  }
-
-  set {
-    name  = "resources.limits.memory"
-    value = "1Gi"
-  }
-
-  depends_on = [var.eks_cluster_name]
-}
-
-resource "helm_release" "kibana" {
-  name       = "kibana"
-  repository = "https://helm.elastic.co"
-  chart      = "kibana"
-  namespace  = "logging"
-  version    = "8.5.1"
-
-  set {
-    name  = "service.type"
-    value = "NodePort"
-  }
-
-  set {
-    name  = "service.nodePort"
-    value = "32002"
-  }
-
-  depends_on = [helm_release.elasticsearch]
-}
-
-resource "helm_release" "logstash" {
-  name       = "logstash"
-  repository = "https://helm.elastic.co"
-  chart      = "logstash"
-  namespace  = "logging"
-  version    = "8.5.1"
-
-  depends_on = [helm_release.elasticsearch]
-}
-
-resource "helm_release" "filebeat" {
-  name       = "filebeat"
-  repository = "https://helm.elastic.co"
-  chart      = "filebeat"
-  namespace  = "logging"
-  version    = "8.5.1"
-
-  depends_on = [helm_release.elasticsearch]
 }
